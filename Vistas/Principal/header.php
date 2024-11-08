7e94f10d2215a51709a4125164b25873113958fc
daviju <122981434+daviju@users.noreply.github.com>
2024-11-08 19:35:12 +0100

    Cambio en el navbar

--- Vistas/Principal/header.php
+++ Vistas/Principal/header.php
@@ -1,43 +1,39 @@
-<header>
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e1d8b8;">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Navbar 14</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="../../css/style.css" />
+  </head>
 
-            <img src="./images/Wrap&Roll.png" alt="Logo Kebab" width="100px" height="100px">
+  <body>
+    <div class='tusmuertos'> 
+        <img src="./images/Wrap&Roll.png" alt="Logo Kebab" width="100px" height="100px" style="border-radius: 50%; object-fit: cover;">
+    <nav>
 
-            <a class="navbar-brand" href="#">Wrap 'n' Roll</a>
-            
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+      <a class="item">Home</a>
+      <a class="item">About</a>
+      <div class="item">
+        Skills
+        <div class="dropdown">
+          <div>
+            <a>React</a>
+            <a>Angular</a>
+            <a>Vue</a>
+          </div>
+        </div>
+      </div>
+      <a class="item">Contact</a>
+      <div class="underline"></div>
+    </nav>
+</div>
+  </body>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-            
-                <li class="nav-item active">
-                        <a class="nav-link" href="?menu=mantenimiento">MANTENIMIENTO <span class="sr-only">(current)</span></a>
-                    </li>
-            
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            LISTADOS
-                        </a>
-            
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="?menu=listadoanimales">ANIMALES</a>
-                            <a class="dropdown-item" href="?menu=listadovacunas">VACUNACIONES</a>
-                        </div>
-                    </li>
-                </ul>
-
-                <?= Sesion::existe('login')?"Hola bienvenido ".Sesion::leer('login').
-                "<a href='?menu=cerrarsesion'>Cerrar sesión</a>":""; ?>
-
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                </form>
-
-            </div>
-        </nav>
-    </header>
+</html>
